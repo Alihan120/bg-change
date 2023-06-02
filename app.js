7dbad7cf3c3c7003b9dbd35729da953ea5bd4217
Alihan <alhnkcmn12@gmail.com>
2023-06-02 11:46:26 +0200

    last

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+const btnCLick =document.getElementById("change")
+const btnOver = document.getElementById("over")
+let text = document.querySelector("#text")
+const colorPrint = document.createElement("p")
+text.appendChild(colorPrint)
+
+btnCLick.onclick = () => {
+    let color1 = Math.trunc(Math.random()*255);
+    let color2 = Math.trunc(Math.random()*255);
+    let color3 = Math.trunc(Math.random()*255);
+    document.body.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")";
+    text.innerHTML = document.body.style.backgroundColor;
+}
+
+btnOver.onmouseover = () =>{
+    let color1 = Math.trunc(Math.random()*255);
+    let color2 = Math.trunc(Math.random()*255);
+    let color3 = Math.trunc(Math.random()*255);
+    document.body.style.backgroundColor = "rgb("+color1+","+color2+","+color3+")"; 
+    text.innerHTML = document.body.style.backgroundColor;
+   
+}
